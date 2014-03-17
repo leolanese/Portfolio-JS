7976581a3b18acab96fc95e030e6c2e03f6fcd51
sirwilliam <developer@leolanese.com>
2014-03-17 16:38:50 +0000

    including Source Maps LESS for Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,23 +4,36 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
-        srcPath: './p',
-        tests: './tests',
-        lessPath: './less',
+        // Metadata.
+        meta: {
+            basePath: '../',
+            srcPath: '../src/',
+            deployPath: '../deploy/',
+            jasmine: 'tests/jasmine/',
+            root: '/Users/Leo/Documents/root/rcv',
+            www: '/Users/Leo/Documents/root/rcv/www'
+        },
 
+        // debugging: grunt less:development
         less: {
             // name module
             development: {
                 options: {
                     compress: true,
-                    yuicompress: true,
-                    optimization: 2
+
+                    // To enable, set sourceMap to true and update sourceMapRootpath based on your install
+                    // sourceMap needs grunt-contrib-less > 0.9.0
+                    sourceMap: true,
+                    sourceMapFilename: 'css/main.css.map', // where file is generated and located
+
+                    sourceMapURL: '/rcv/css/main.css.map'
                 },
+
                 files: {
                     // target.css file: source.less file
-                    '/Applications/MAMP/htdocs/p/css/devices.css': '/Applications/MAMP/htdocs/p/less/devices.less',
-                    '/Applications/MAMP/htdocs/p/css/core.css': '/Applications/MAMP/htdocs/p/less/core.less',
-                    '/Applications/MAMP/htdocs/p/css/portfolio.css': '/Applications/MAMP/htdocs/p/less/portfolio.less'
+                    '<%= meta.root  %>/css/devices.css': '<%= meta.root  %>/less/devices.less',
+                    '<%= meta.root  %>/css/core.css': '<%= meta.root  %>/less/core.less',
+                    '<%= meta.root  %>/css/portfolio.css': '<%= meta.root  %>/less/portfolio.less'
                 }
             }
         },
@@ -34,11 +47,21 @@ module.exports = function(grunt) {
             files: ['Gruntfile.js', './js/**/*.js']
         },
 
+        jasmine: {
+            pivotal: {
+                src: '<%= jasmine =>/src/**/*.js',
+                options: {
+                    specs: '<%= jasmine =>/spec/*Spec.js',
+                    helpers: '<%= jasmine =>/spec/*Helper.js'
+                }
+            }
+        },
+
         watch: {
             // Watch for LESS changes, building CSS directly
             styles: {
                 // Which files to watch (all .less files recursively in the less directory)
-                files: ['/Applications/MAMP/htdocs/p/less/**/*.less'],
+                files: ['<%= meta.root  %>/less/**/*.less'],
                 tasks: ['less'],
                 options: {
                     nospawn: true
@@ -46,23 +69,23 @@ module.exports = function(grunt) {
             },
             // Watch for JS changes, linting the JS and copying direct to deployment directory.
             scripts: {
-                files: ['Gruntfile.js', 'server.js', '/Applications/MAMP/htdocs/p/**/*.js', '/Applications/MAMP/htdocs/p/tests/**/*.js'],
-                tasks: ['jshint']
+                files: ['Gruntfile.js', 'server.js', '<%= meta.www  %>/**/*.js', '<%= meta.www  %>/tests/**/*.js'],
+                tasks: ['less::development' ]
             }
 
         }
 
-
-
     });
 
     // Load required modules
-    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
 
     // Task definitions
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('default', ['jshint', 'haml']);
+ //   grunt.registerTask('default', ['jshint']);
 
 };
